1d39bb13a81d37864eeb70027e49e74b74e2f44f
DejanPeric058 <najedcirep@gmail.com>
2024-12-04 18:29:25 +0100

    Day 4.

--- 04.py
+++ 04.py
@@ -56,5 +56,26 @@ for i, row in enumerate(c):
         counter += prestej_xmas(i,j,a)
 print(counter)
 
+counter = 0
 
-#aaaa
+def prestej_x_mas(i,j,a):
+    if a == 'A':
+        try:
+            niz1 = c[i-1][j-1] + c[i+1][j+1]
+            niz2 = c[i-1][j+1] + c[i+1][j-1]
+            niz1 = ''.join(sorted(niz1))
+            niz2 = ''.join(sorted(niz2))
+        except:
+            niz1 = 'MM'
+            niz2 = 'MM'
+        if i!=0 and j!=0 and niz1 == 'MS' and niz2 == 'MS':
+            return 1
+        else:
+            return 0
+    else:
+        return 0
+
+for i, row in enumerate(c):
+    for j, a in enumerate(row):
+        counter += prestej_x_mas(i,j,a)
+print(counter)
